99d3b4493d7ae1faa91def69401da89c1503ec57
paul <paul@chibi.ubc.ca>
2014-03-29 11:25:21 -0700

    revert

--- gsec/src/main/resources/sql/init-acl-indices.sql
+++ gsec/src/main/resources/sql/init-acl-indices.sql
@@ -1,8 +1,8 @@
-drop table if exists ACLHIBERNATESEQUENCES;
-create table ACLHIBERNATESEQUENCES (
-	sequence_name VARCHAR(255) character set latin1 not null,
-	sequence_next_hi_value BIGINT not null
-);
+--drop table if exists ACLHIBERNATESEQUENCES;
+--create table ACLHIBERNATESEQUENCES (
+	--sequence_name VARCHAR(255) character set latin1 not null,
+--	sequence_next_hi_value BIGINT not null
+--);
 
 alter table ACLOBJECTIDENTITY add unique key acloid (OBJECT_CLASS,OBJECT_ID);
 alter table ACLOBJECTIDENTITY add key objectclasskey (OBJECT_CLASS);
